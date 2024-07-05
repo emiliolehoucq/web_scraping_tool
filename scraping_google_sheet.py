73353eeb1648ad8ba58636433ec6b81e36549b23
Emilio Lehoucq <emiliolehoucq@gmail.com>
2024-07-05 10:50:55 -0500

    Cleaning up code

--- scraping_google_sheet.py
+++ scraping_google_sheet.py
@@ -3,31 +3,19 @@
 
 # Loading libraries
 from googleapiclient.discovery import build
-from google.oauth2.service_account import Credentials
 from google.oauth2 import service_account
 import pandas as pd
 from scrapers import get_selenium_response
 import os
 import json
+from datetime import datetime
 
 # Get URLs from the Google Sheet
 
-# # For local machine:
-# # In the local machine, run: export GOOGLE_APPLICATION_CREDENTIALS="credentials.json"
-# # Get the credentials from the environment variable
-# creds, _ = google.auth.default()
-
-# For GitHub Actions:
-# Store credentials in secrets
-# # Write the credentials from the environment variable to a file
-# with open('credentials.json', 'w') as creds_file:
-#     creds_file.write(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-
-# # Load credentials from the file
-# creds = Credentials.from_service_account_file('credentials.json')
-
-# Retrieve the environment variable and parse it as JSON
+# Retrieve the environment variable with the credentials
 credentials_info = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+# Parse credentials as JSON
 credentials_dict = json.loads(credentials_info)
 
 # Use the parsed dictionary to create the credentials object
@@ -48,6 +36,9 @@ df = pd.read_csv('data.csv')
 # Get existing URLs
 existing_urls = df['url'].tolist()
 
+# Get current timestamp
+ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
 # Iterate over the URLs in the Google Sheet
 for row in rows:
     # Check if the given URL is already in the CSV
@@ -56,7 +47,7 @@ for row in rows:
         response = get_selenium_response(row[0])
         # If the response is not None, add it to the CSV
         if response is not None:
-            df = pd.concat([df, pd.DataFrame({'url': row[0], 'html': response}, index=[len(df)])], ignore_index=True)
+            df = pd.concat([df, pd.DataFrame({'ts': ts, 'url': row[0], 'html': response}, index=[len(df)])], ignore_index=True)
     # If it's already in the CSV, skip it
     else:
         continue
